03ef696def23664fe15f8db05e7e621ad8fc3aa5
d1f0lt01 <d1f0lt01@gmail.com>
2025-05-18 17:15:39 +0300

    Added: base ui

--- app/src/main/java/com/example/weatherapp/screens/MainScreen.kt
+++ app/src/main/java/com/example/weatherapp/screens/MainScreen.kt
@@ -1,25 +1,32 @@
 package com.example.weatherapp.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.weatherapp.R
-import com.example.weatherapp.ui.theme.BlueSoft
-import com.example.weatherapp.ui.theme.Purple40
 
 @Preview(showBackground = true)
 @Composable
@@ -28,8 +35,7 @@ fun MainScreen() {
         painter = painterResource(id = R.drawable.bg),
         contentDescription = "Background",
         modifier = Modifier
-            .fillMaxSize()
-            .alpha(0.7f),
+            .fillMaxSize(),
         contentScale = ContentScale.Crop
     )
     Column(
@@ -38,16 +44,82 @@ fun MainScreen() {
             .padding(5.dp)
     ) {
         Card(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxWidth(),
             colors = CardDefaults.cardColors(
-                containerColor = BlueSoft
+                containerColor = Color.Transparent,
             ),
             elevation = CardDefaults.cardElevation(
                 defaultElevation = 0.dp
             ),
-            shape = RoundedCornerShape(20.dp)
+            shape = RoundedCornerShape(8.dp)
         ) {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(top = 3.dp, bottom = 3.dp, start = 10.dp, end = 10.dp),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.spacedBy(50.dp),
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                ) {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.plus),
+                            contentDescription = "Plus",
+                            modifier = Modifier.size(36.dp),
+                            tint = Color.White,
+                        )
+                    }
+                }
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(5.dp),
+                ) {
+                    Text(
+                        text = "Moscow",
+                        style = TextStyle(fontSize = 42.sp),
+                        color = Color.White
+                    )
+                    Text(
+                        text = "23°C",
+                        style = TextStyle(fontSize = 108.sp),
+                        color = Color.White
+                    )
+                    Text(
+                        text = "Clear 19°/8°",
+                        style = TextStyle(fontSize = 32.sp),
+                        color = Color.White
+                    )
 
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Last update: 18 May 15:23",
+                            style = TextStyle(fontSize = 16.sp),
+                            color = Color.White
+                        )
+                        IconButton(
+                            onClick = {}
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.sync),
+                                contentDescription = "Sync",
+                                modifier = Modifier.size(36.dp),
+                                tint = Color.White,
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
@@ -10,4 +10,4 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val BlueSoft = Color(0xFF2196F3)
+val BlueSoft = Color(0xBF2196F3)
